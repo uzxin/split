2dbd511c969da47ed18b99dce37b3f6118c80c40
uzxin <125897228@qq.com>
2021-01-04 11:36:27 +0800

    Initial commit

---
+++ src/test/java/com/uzxin/split/SplitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uzxin.split;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
